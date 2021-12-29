c6cb8e620f07cea27a4a9a3cbbefdd18ffdaf51c
thorsten <thorsten.steuer@gmx.net>
2021-12-29 13:25:20 +0100

    update

--- test_enviornment/dockerfiles/ubuntu_mosquitto_broker/resource_monitoring_v1.sh
+++ test_enviornment/dockerfiles/ubuntu_mosquitto_broker/resource_monitoring_v1.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 SERVICE="mosquitto"
 while true ; do
-    sleep 1	
-    echo "$SERVICE"
     mosquitto_cpu_usage=$(top -b -n 1 | grep mosquitto | awk '{print $9}')		
-    echo "$mosquitto_cpu_usage"
     int_cpu=${mosquitto_cpu_usage%%.*}
-    if [ "$int_cpu" -gt 10 ]
+    if [ "$int_cpu" -gt 90 ]
         then
             echo "Borker uses to much CPU killing process"
             pkill $SERVICE
--- test_enviornment/dockerfiles/ubuntu_mosquitto_broker/start_v1.sh
+++ test_enviornment/dockerfiles/ubuntu_mosquitto_broker/start_v1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mosquitto -c /var/lib/mosquitto/mosquitto.conf &
+mosquitto -c /var/lib/mosquitto/mosquitto.conf
 
 
 
